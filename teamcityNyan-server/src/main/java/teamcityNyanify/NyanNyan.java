b59e23dae1637a436c84772157420a8965453c91
Igor Shevchenko <cyberskunk@gmail.com>
2016-04-06 16:46:49 +0300

    do not nyanify if corresponding internal property is not present

--- teamcityNyan-server/src/main/java/teamcityNyanify/NyanNyan.java
+++ teamcityNyan-server/src/main/java/teamcityNyanify/NyanNyan.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 public class NyanNyan extends SimplePageExtension {
     public NyanNyan(@NotNull PagePlaces pagePlaces) {
         super(pagePlaces, PlaceId.ALL_PAGES_HEADER, "teamcityNyanify", "nyanify.jsp");
-        if (TeamCityProperties.getBooleanOrTrue("teamcity.ui.nyanify")) {
+        if (TeamCityProperties.getBoolean("teamcity.ui.nyanify")) {
             addCssFile("nyan-nyan.css");
         }
         register();
